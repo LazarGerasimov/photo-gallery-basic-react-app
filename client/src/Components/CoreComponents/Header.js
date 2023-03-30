e4279e85a8ad23f6dad9b1ad2f0bf2c84c40d5a2
Laz <lazgerasimov@gmail.com>
2023-03-30 13:03:47 +0100

    add home page comp, add home page css

--- client/src/Components/CoreComponents/Header.js
+++ client/src/Components/CoreComponents/Header.js
@@ -9,7 +9,7 @@ export const Header = () => {
                 <div className="logo-wrap">
                     <p className="logo">
                         <Link>
-                            <img src="/images/logo3.png" />
+                            <img src="/images/logo3.png" alt='camera-logo'/>
                         </Link>
                     </p>
                 </div>
--- client/src/Components/CoreComponents/HomePage.js
+++ client/src/Components/CoreComponents/HomePage.js
@@ -1,8 +1,23 @@
-
+import { Link } from "react-router-dom"
+import './HomePage.css';
 
 
 export const HomePage = () => {
     return (
-        <h1>Home Page</h1>
+        <>
+
+            {/* if !user */}
+            <div className="home-container">
+                <h2>Check our latest photos <Link className="link" to="/photos/most-recent">here</Link> </h2>
+                <h1>Welcome to our gallery</h1>
+            </div>
+
+            {/* if user */}
+            {/* <div className="home-container">
+                <h2>Check our latest photos <Link to="/photos">here</Link> </h2>
+                <h1>Welcome to our gallery</h1>
+            </div> */}
+
+        </>
     )
 }
