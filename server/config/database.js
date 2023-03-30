52628f0c8746d45324398f7cdc6c0d305cf1757d
Laz <lazgerasimov@gmail.com>
2023-03-30 10:47:06 +0100

    test db

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); // mongoose 7 new req
 
 //TODO change db according to assignment
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/scaffoldDb2'
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/react-photos'
 
 
 module.exports = async (app) => {
