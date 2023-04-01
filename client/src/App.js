f9f204ca4e1df68fbb16301bb80dd0bae969e1d8
Laz <lazgerasimov@gmail.com>
2023-04-01 16:28:08 +0100

    move hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import { AuthContext } from './contexts/AuthContext';
-import { useLocalStorage } from './Components/hooks/useLocalStorage';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Header } from './Components/CoreComponents/Header/Header';
 import { HomePage } from './Components/CoreComponents/HomePage/HomePage';
 import { Login } from './Components/AuthComponents/Login/Login';
