618b4a2a3b2a206f25b5dc972840c8455db8b194
Laz <lazgerasimov@gmail.com>
2023-04-01 14:42:15 +0100

    add context provider in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
 
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { AuthContext } from './contexts/AuthContext';
 
 import { Header } from './Components/CoreComponents/Header/Header';
 import { HomePage } from './Components/CoreComponents/HomePage/HomePage';
 import { Login } from './Components/AuthComponents/Login/Login';
 import { Register } from './Components/AuthComponents/Register/Register';
+import './App.css';
+
 
 function App() {
-  return (
-    <>
-      <Header />
-        <Routes>
-            <Route path='/' element={<HomePage />}/>
-            <Route path='/auth/login' element={<Login />}/>
-            <Route path='/auth/register' element={<Register />}/>
-        </Routes>
-    </>
-  );
+    return (
+        <AuthContext.Provider>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/auth/login' element={<Login />} />
+                <Route path='/auth/register' element={<Register />} />
+            </Routes>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
