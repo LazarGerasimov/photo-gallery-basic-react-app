ef46eefeaae043d89f41113d0ca2f0aff2e0d7a3
Laz <lazgerasimov@gmail.com>
2023-04-01 14:24:40 +0100

    add login & register services

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,50 @@
+const baseUrl = `http://localhost:3030`;
 
 
+export const login = async (data) => {
+
+    try {
+        const response = await fetch(`${baseUrl}/auth/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        const result = await response.json();
+
+        if (response.ok) {
+            return result
+        } else {
+            throw new Error(result.error);
+        }
+
+    } catch (error) {
+        return error
+    }
+}
 
 
-const baseUrl = `http://localhost:3000`;
+export const register = async (data) => {
+
+    try {
+        const response = await fetch(`${baseUrl}/auth/register`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            return result
+        } else {
+            throw new Error(result.error);
+        }
+
+    } catch (error) {
+        return error
+    }
+
+}
