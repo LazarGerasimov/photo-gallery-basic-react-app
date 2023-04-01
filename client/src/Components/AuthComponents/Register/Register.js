d857248da84cde21e7775e0b4d143c82b279794c
Laz <lazgerasimov@gmail.com>
2023-04-01 14:52:03 +0100

    test reg form state

--- client/src/Components/AuthComponents/Register/Register.js
+++ client/src/Components/AuthComponents/Register/Register.js
@@ -10,32 +10,48 @@ export const Register = () => {
         password: ''
     });
 
-    
+    const onChangeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+        console.log(formData);
+    }
+
 
     return (
-            <div className={styles["log-form"]}>
-                <h2>Register a new account</h2>
-                <form className={styles["register"]}>
+        <div className={styles["log-form"]}>
+            <h2>Register a new account</h2>
+            <form className={styles["register"]}>
 
-                    <input type="text" title="email" placeholder="email" name="email" />
+                <input
+                    name="email"
+                    type="text"
+                    title="email"
+                    placeholder="email"
+                    onChange={onChangeHandler}
+                />
 
-                    {/* <p className={styles["error"]}>
+                {/* <p className={styles["error"]}>
                         Email is required!
                     </p> */}
 
-                    <input type="password" placeholder="password" name="password" />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                />
 
-                    {/* <p className="error">
+                {/* <p className="error">
                     Password is required!
                     </p> */}
 
-                    {/* <p className="error">
+                {/* <p className="error">
                     Password must be at least 5 symbols!
                     </p> */}
 
-                    <button type="submit" className={styles["btn"]}>Register</button>
-                    <Link className={styles["existing"]} to="/auth/login">Already a member? Login Here!</Link>
-                </form>
-            </div >        
+                <button type="submit" className={styles["btn"]}>Register</button>
+
+                <Link className={styles["existing"]} to="/auth/login">Already a member? Login Here!</Link>
+
+            </form>
+        </div >
     )
 }
