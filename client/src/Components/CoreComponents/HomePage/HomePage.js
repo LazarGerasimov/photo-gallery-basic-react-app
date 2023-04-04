c65afe0d5ddc7f9357e2bb5067fb7b866ce3366c
Laz <lazgerasimov@gmail.com>
2023-04-04 13:30:18 +0100

    update user/guest link on HomePage

--- client/src/Components/CoreComponents/HomePage/HomePage.js
+++ client/src/Components/CoreComponents/HomePage/HomePage.js
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom"
 import styles from './HomePage.module.css';
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 
 export const HomePage = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <>
 
             {/* if !user */}
             <div className={styles["home-container"]}>
-                <h2>Check our latest photos <Link className={styles["link"]} to="/photos/most-recent">here</Link> </h2>
+
+                {!user
+                    ?
+                    <h2>Check our latest photos <Link className={styles["link"]} to="/photos/most-recent">here</Link> </h2>
+                    :
+                    <h2>Check our latest photos <Link className={styles["link"]} to="/photos">here</Link> </h2>
+                }
+
+
                 <div className={styles["welcome-wrapper"]}>
                     <div className={styles["inner-welcome-wrapper"]}>
                         <span>Welcome</span>
