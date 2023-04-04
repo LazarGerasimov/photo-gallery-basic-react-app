75c72729007ad07b83a76e4422e5af589cf7aef3
Laz <lazgerasimov@gmail.com>
2023-04-04 11:30:51 +0100

    test RecentPhotos

--- client/src/Components/PhotoComponents/RecentPhotos/RecentPhotos.js
+++ client/src/Components/PhotoComponents/RecentPhotos/RecentPhotos.js
@@ -1,11 +1,22 @@
 
 
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as apiService from '../../../services/apiService';
 import styles from './RecentPhotos.module.css';
 
 export const RecentPhotos = () => {
+
     const [recentPhotos, setRecentPhotos] = useState([]);
+
+    useEffect(() => {
+        apiService.getRecentPhotos()
+            .then(data => {
+                setRecentPhotos(data);
+                console.log(data);
+            })
+    }, []);
+
     return (
         <h1>Recent Photos</h1>
     )
