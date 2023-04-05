7b1eeac82a7cd306345e2583d5d9f7079ac7d144
Laz <lazgerasimov@gmail.com>
2023-04-05 09:02:07 +0100

    add PhotoDetails comp

--- client/src/Components/PhotoComponents/PhotoDetails/PhotoDetails.js
+++ client/src/Components/PhotoComponents/PhotoDetails/PhotoDetails.js
@@ -1,24 +1,39 @@
 import { useEffect, useState } from "react"
 import * as apiService from '../../../services/apiService';
 import { useParams } from "react-router-dom";
+import styles from './PhotoDetails.module.css';
 
 
 export const PhotoDetails = () => {
 
     const [photo, setPhoto] = useState();
-    const {photoId} = useParams();
+    const { photoId } = useParams();
 
     useEffect(() => {
         apiService.getPhotoById(photoId)
             .then(data => {
-                setPhoto(photo);
+                setPhoto(data);
                 console.log(photo);
             })
     }, [])
 
 
-
     return (
-        <h1>{photoId}</h1>
+        <div className={styles["photo-details"]}>
+            <div className={styles["photo-details-wrapper"]}>
+                <figure>
+                    <img src={photo.imageUrl} alt="img" />
+                </figure>
+                <h3>{photo?.title}</h3>
+                <h2>£{photo?.price}</h2>
+                <p>{photo?.description}</p>
+            </div>
+
+            {/* <button type="submit" className="btn">Like</button> */}
+
+            <button type="submit" className="btn"> Edit</button >
+            <button type="submit" className="btn"> Delete</button >
+
+        </div >
     )
 }
