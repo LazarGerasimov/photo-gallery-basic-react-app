f6f338257827db8fb617fdbf30af39e9c2a5629a
Laz <lazgerasimov@gmail.com>
2023-04-07 18:53:58 +0100

    login form validation complete

--- client/src/Components/AuthComponents/Login/Login.js
+++ client/src/Components/AuthComponents/Login/Login.js
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../contexts/AuthContext';
 
 import styles from './Login.module.css';
@@ -28,13 +28,20 @@ export const Login = () => {
             const emailRegexValidator = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/;
             if (!e.target.value.match(emailRegexValidator)) {
                 setErrors(errors => ({ ...errors, [e.target.name]: true }));
+            } else {
+                setErrors(errors => ({ ...errors, [e.target.name]: false }));
+
             }
         }
         if (e.target.name === 'password') {
             if (e.target.value.length < 6 || e.target.value.length > 12) {
                 setErrors(errors => ({ ...errors, [e.target.name]: true }));
+            } else {
+                setErrors(errors => ({ ...errors, [e.target.name]: false }));
             }
         }
+
+
     }
 
     const { setUserData } = useContext(AuthContext);
@@ -45,6 +52,10 @@ export const Login = () => {
 
         try {
             const userData = await userService.login(formData);
+            if (userData?.message) {
+                console.log(userData.message);
+                return setErrors({ ...errors, serverErrors: userData.message });
+            }
             setUserData(userData);
             navigate('/');
         } catch (error) {
@@ -55,40 +66,50 @@ export const Login = () => {
 
 
     return (
+        <>
+            <div className={styles["log-form"]}>
+                <h2>Login to your account</h2>
+                <form className={styles.login} onSubmit={onSubmitHandler}>
 
-        <div className={styles["log-form"]}>
-            <h2>Login to your account</h2>
-            <form className={styles.login} onSubmit={onSubmitHandler}>
+                    <input
+                        name='email'
+                        type="text"
+                        title="email"
+                        placeholder="email"
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
+                    />
 
-                <input
-                    name='email'
-                    type="text"
-                    title="email"
-                    placeholder="email"
-                    onChange={onChangeHandler}
-                />
-                {/* <p className="error" >
-                    Email is required!
-                </p> */}
 
-                <input
-                    name='password'
-                    type="password"
-                    placeholder="password"
-                    onChange={onChangeHandler}
-                />
+                    <input
+                        name='password'
+                        type="password"
+                        placeholder="password"
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
+                    />
 
-                {/* <p className="error" >
-                    Password is required!
-                </p> */}
-
-                {/* <p className="error" >
-                    Password must be at least 5 symbols!
-                </p> */}
-
-                <button type="submit" className={styles.btn}>Login</button>
-                <Link className={styles.existing} to="/auth/register">Not a member? Register Here!</Link>
-            </form >
-        </div >
+                    <button type="submit" className={styles.btn}>Login</button>
+                    <Link className={styles.existing} to="/auth/register">Not a member? Register Here!</Link>
+                </form >
+            </div >
+            {(errors.email || errors.password || errors.serverErrors) &&
+                <div className={styles["errors"]}>
+                    {errors.email &&
+                        <p className="error" >
+                            Valid email required!
+                        </p>}
+                    {errors.password &&
+                        <p className="error" >
+                            Password must be between 6 and 12 characters!
+                        </p>}
+                    {errors.serverErrors &&
+                        <p className="error" >
+                            {errors.serverErrors}
+                        </p>}
+                </div>
+            }
+        </>
     )
+
 }
